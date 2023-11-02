b802a107387ce4ceeea23693a6d285f63f1b9b71
arup <arup.debnath3@gmail.com>
2023-11-02 18:37:14 +0530

    Update

--- components/forms/AddMatchForm.tsx
+++ components/forms/AddMatchForm.tsx
@@ -1,22 +1,36 @@
 'use client'
 
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Teams } from "@/utils/Teams"
+import { cn } from "@/utils/shadcnUtils"
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useEffect, useState } from 'react'
+import { CalendarIcon } from "@radix-ui/react-icons"
+import { format } from "date-fns"
+import React, { useState } from "react"
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import CenteredArea from '../customUi/CenteredArea'
 import { Button } from '../ui/button'
+import { Calendar } from "../ui/calendar"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
 import { Input } from "../ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
-import { Teams } from "@/utils/Teams"
+import { Textarea } from "../ui/textarea"
 
 const formSchema = z.object({
     teamA: z.enum(Teams),
     teamB: z.enum(Teams),
     batFirst: z.enum(Teams),
-    location: z.string().trim().min(3, 'Minimum 3 chars')
+    location: z.string().trim().min(3, 'Minimum 3 chars'),
+    matchDate: z.date().refine((date) => new Date(date).toString() !== 'Invalid Date', {
+        message: 'Date is not valid'
+    }).transform((date) => new Date(date)),
+
+    sessionAbat: z.string().trim().min(1, 'Enter valid details'),
+    sessionAbowl: z.string().trim().min(1, 'Enter valid details'),
+    sessionBbat: z.string().trim().min(1, 'Enter valid details'),
+    sessionBbowl: z.string().trim().min(1, 'Enter valid details'),
 })
 
 const AddMatchForm = () => {
@@ -32,7 +46,12 @@ const AddMatchForm = () => {
             teamA: '',
             teamB: '',
             batFirst: '',
-            location: ''
+            location: '',
+            matchDate: new Date(),
+            sessionAbat: '',
+            sessionAbowl: '',
+            sessionBbat: '',
+            sessionBbowl: '',
         },
         mode: 'onBlur',
         resolver: zodResolver(formSchema)
@@ -63,11 +82,13 @@ const AddMatchForm = () => {
                                 <CardTitle>Card Title</CardTitle>
                                 <CardDescription>Card Description</CardDescription>
                             </CardHeader>
+
+
                             <CardContent>
                                 <div className="space-y-3">
                                     <div>
                                         <Select
-                                            onValueChange={(selectedValue) => {
+                                            onValueChange={(selectedValue: string) => {
                                                 setValue('teamA', selectedValue)
                                                 setBatFirstValues(prevState => ({ ...prevState, teamA: watch().teamA }))
                                             }}
@@ -88,7 +109,7 @@ const AddMatchForm = () => {
 
                                     <div>
                                         <Select
-                                            onValueChange={(selectedValue) => {
+                                            onValueChange={(selectedValue: string) => {
                                                 setValue('teamB', selectedValue)
                                                 setBatFirstValues(prevState => ({ ...prevState, teamB: watch().teamB }))
                                             }}
@@ -108,7 +129,7 @@ const AddMatchForm = () => {
 
                                     <div>
                                         <Select
-                                            onValueChange={(selectedValue) => setValue('batFirst', selectedValue)}
+                                            onValueChange={(selectedValue: string) => setValue('batFirst', selectedValue)}
                                         >
                                             <SelectTrigger>
                                                 <SelectValue placeholder="Bat First" />
@@ -138,22 +159,105 @@ const AddMatchForm = () => {
                                             </p>
                                         )}
                                     </div>
+
+                                    <div>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button
+                                                    variant={"outline"}
+                                                    className={cn(
+                                                        "w-[240px] justify-start text-left font-normal min-w-full",
+                                                        !watch().matchDate && "text-muted-foreground"
+                                                    )}
+                                                >
+                                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                                    {watch().matchDate ? format(watch().matchDate as any, "PPP") : <span>Pick a date</span>}
+                                                </Button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={watch().matchDate as Date}
+                                                    onSelect={(selectedValue: Date | undefined) => {
+                                                        setValue('matchDate', selectedValue as Date)
+                                                    }}
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                    </div>
                                 </div>
                             </CardContent>
+
+
                             <CardFooter>
                                 <Button>Submit</Button>
                                 <Button onClick={() => handleChangeTab("1")}>Next</Button>
                             </CardFooter>
                         </Card>
                     </TabsContent>
+
                     <TabsContent value={"1"}>
                         <Card>
                             <CardHeader>
                                 <CardTitle>Card Title</CardTitle>
                                 <CardDescription>Card Description</CardDescription>
                             </CardHeader>
+
                             <CardContent>
+                                <div className="space-y-3">
+                                    <div>
+                                        <Textarea
+                                            rows={8}
+                                            placeholder="Enter Session A Batting Details Followed by New Lines.."
+                                            {...register('sessionAbat')}
+                                        />
+                                    </div>
+
+
+                                    <div>
+                                        <Textarea
+                                            rows={8}
+                                            placeholder="Enter Session A Bowling Details Followed by New Lines.."
+                                            {...register('sessionAbowl')}
+                                        />
+                                    </div>
+                                </div>
                             </CardContent>
+
+                            <CardFooter>
+                                <Button>Submit</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+
+                    <TabsContent value={"2"}>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Card Title</CardTitle>
+                                <CardDescription>Card Description</CardDescription>
+                            </CardHeader>
+
+                            <CardContent>
+                                <div className="space-y-3">
+                                    <div>
+                                        <Textarea
+                                            rows={8}
+                                            placeholder="Enter Session B Batting Details Followed by New Lines.."
+                                            {...register('sessionBbat')}
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <Textarea
+                                            rows={8}
+                                            placeholder="Enter Session B Bowling Details Followed by New Lines.."
+                                            {...register('sessionBbowl')}
+                                        />
+                                    </div>
+                                </div>
+                            </CardContent>
+
                             <CardFooter>
                                 <Button>Submit</Button>
                             </CardFooter>
