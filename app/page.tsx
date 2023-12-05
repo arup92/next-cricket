769f281376c1230fdde77d649b0dc30c1a173a7c
arup92 <arup.debnath3@gmail.com>
2023-12-05 14:50:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Script from 'next/script';
 export default function Home() {
   return (
     <>
-        <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-5H0Z2H7XJ6"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
