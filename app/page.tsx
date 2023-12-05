8e0c2cb565e55a576a022cc106261240f13c0129
arup <arup.debnath3@gmail.com>
2023-12-05 14:55:38 +0530

    udpate

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Script from 'next/script';
 export default function Home() {
   return (
     <>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5H0Z2H7XJ6" />
-      <Script>
+      <Script id="gtm-script" async src="https://www.googletagmanager.com/gtag/js?id=G-5H0Z2H7XJ6" />
+      <Script id="gtm-script-code">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
