ad3e63c608dd42956987f929531c35d9cdd814b8
arup92 <arup.debnath3@gmail.com>
2023-12-09 21:41:03 +0530

    update

--- app/view/venue/[venueid]/page.tsx
+++ app/view/venue/[venueid]/page.tsx
@@ -1,5 +1,21 @@
 import CenteredArea from "@/components/customUi/CenteredArea"
 import VenueStats from "@/components/view/VenueStats"
+import { capitalizeFullName } from "@/utils/style"
+import { Metadata } from "next"
+
+type Props = {
+    params: { venueid: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    let title = `${capitalizeFullName(params.venueid)} Most Recent Statstics | ${process.env.APP_NAME}`
+    let description = `${capitalizeFullName(params.venueid)} Most Recent International Cricket Statstics | ${process.env.APP_NAME}`
+
+    return {
+        title,
+        description
+    }
+}
 
 const Venue = ({ params }: { params: { venueid: string } }) => {
     return (
