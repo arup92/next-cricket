984e546b9ad29d25cbbfd7ce5bb6fdde67ebe8b6
arup <arup.debnath3@gmail.com>
2023-12-16 17:48:59 +0530

    update match

--- app/api/view/match-get/route.ts
+++ app/api/view/match-get/route.ts
@@ -11,65 +11,40 @@ export async function GET(request: Request) {
     }
 
     try {
-        // Find match
         const match = await prismaClient.match.findUnique({
             where: {
                 id: parseInt(matchId)
             },
-            select: {
-                batFirst: true,
-                id: true,
-                matchDate: true,
-                matchFormat: true,
-                result: true,
-                teamAId: true,
-                teamBId: true,
-                venueId: true,
-                venue: {
-                    select: {
-                        venueCountryId: true,
-                    }
-                }
+            include: {
+                Scores: true,
+                batting: true,
+                bowling: true,
+                venue: true
             }
         })
 
-        // Find Scores
-        const scores = await prismaClient.scores.findMany({
-            where: {
-                matchId: parseInt(matchId)
-            }
-        })
+        const battingA = match?.batting.filter(item => item.teamId === match?.teamAId)
+        const battingB = match?.batting.filter(item => item.teamId === match?.teamBId)
 
-        // Find batting
-        const batting = await prismaClient.batting.findMany({
-            where: {
-                matchId: parseInt(matchId as string)
-            },
-            orderBy: [
-                { run: 'desc' }
-            ]
-        })
-
-        const battingA = batting.filter(item => item.teamId === match?.teamAId)
-        const battingB = batting.filter(item => item.teamId === match?.teamBId)
-
-        // Find bowling
-        const bowling = await prismaClient.bowling.findMany({
-            where: {
-                matchId: parseInt(matchId as string)
-            },
-            orderBy: [
-                { wicket: 'desc' }
-            ]
-        })
-
-        const bowlingA = bowling.filter(item => item.teamId === match?.teamAId)
-        const bowlingB = bowling.filter(item => item.teamId === match?.teamBId)
+        const bowlingA = match?.bowling.filter(item => item.teamId === match?.teamAId)
+        const bowlingB = match?.bowling.filter(item => item.teamId === match?.teamAId)
 
         // Combine the values
         const returnData = {
-            match,
-            scores,
+            match: {
+                id: match?.id,
+                batFirst: match?.batFirst,
+                matchDate: match?.matchDate,
+                matchFormat: match?.matchFormat,
+                result: match?.result,
+                teamAId: match?.teamAId,
+                teamBId: match?.teamBId,
+                venueId: match?.venueId,
+                venue: {
+                    venueCountryId: match?.venue.venueCountryId
+                }
+            },
+            scores: match?.Scores,
             batting: {
                 battingA,
                 battingB
