e0a3b407eb6384fbf8e184233ea381db9331a065
arup92 <arup.debnath3@gmail.com>
2024-01-02 01:46:50 +0530

    update

--- utils/style.ts
+++ utils/style.ts
@@ -11,8 +11,7 @@ export const fantasyPointColor = (point: number, matchFormat: string): string =>
             }
             break;
 
-        case 'T20':
-        case 'IPL':
+        default:
             if (point > 300) {
                 color = 'bg-emerald-700 text-white shadow'
             } else if (point > 220) {
@@ -20,9 +19,6 @@ export const fantasyPointColor = (point: number, matchFormat: string): string =>
             } else if (point > 180) {
                 color = 'bg-emerald-50 text-emerald-700 shadow'
             }
-            break;
-
-        default:
             break;
     }
 
