f699d462b7e9f7b1f509df9cfdd6d775029365fe
arup92 <arup.debnath3@gmail.com>
2024-03-18 09:53:09 +0530

    Update route.ts

--- app/api/user/get-saved-team/route.ts
+++ app/api/user/get-saved-team/route.ts
@@ -11,29 +11,31 @@ export async function GET(request: Request) {
         const savedTeam = await prismaClient.savedTeam.findUnique({
             where: {
                 shortUrl
-            },
-            include: {
-                savedTeamPlayers: {
-                    where: {
-                        Player: {
-                            playerTeams: {
-                                some: {
-                                    active: 'yes'
-                                }
-                            }
-                        }
-                    }
-                },
-                teamA: true,
-                teamB: true
             }
         })
 
-        if (!savedTeam) {
-            return new NextResponse(ErrorMessage.BAD_REQUEST, { status: 400 })
+        if (!!savedTeam) {
+            const savedTeamPlayers = await prismaClient.savedTeamPlayers.findMany({
+                where: {
+                    savedTeamId: savedTeam.id,
+                    Player: {
+                        playerTeams: {
+                            some: {
+                                active: 'yes',
+                                OR: [
+                                    { teamId: savedTeam.teamAId },
+                                    { teamId: savedTeam.teamBId }
+                                ]
+                            }
+                        }
+                    }
+                }
+            })
+
+            return NextResponse.json(savedTeamPlayers, { status: 200 })
         }
 
-        return NextResponse.json(savedTeam.savedTeamPlayers, { status: 200 })
+        return new NextResponse(ErrorMessage.BAD_REQUEST, { status: 400 })
     } catch (error) {
         console.log(error)
         return new NextResponse(ErrorMessage.INT_SERVER_ERROR, { status: 500 })
